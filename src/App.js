18a0c982ea6596e5d05caa78b3d317fe0fd5efe0
lulu <lulu.li@titansoft.com.sg>
2024-05-06 01:07:13 +1000

    add background img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "./App.css";
-
 import ActionProvider from "./ActionProvider";
 import MessageParser from "./MessageParser";
 import config from "./config";
@@ -9,13 +8,14 @@ import config from "./config";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <img src="/FT-Postpartum-Depression.png" />
+      <div className="Chatbot">
         <Chatbot
           config={config}
           actionProvider={ActionProvider}
           messageParser={MessageParser}
         />
-      </header>
+      </div>
     </div>
   );
 }
