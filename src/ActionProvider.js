7d6b99a06d94d008d4a9fd43ad844c4936aa7991
lulu <lulu.li@titansoft.com.sg>
2024-05-06 13:02:05 +1000

    add send api

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -29,6 +29,9 @@ class ActionProvider {
 
     this.updateChatbotState(message);
   };
+  handleResponse=(msg)=>{
+    this.updateChatbotState(msg);
+  }
 
   updateChatbotState(message) {
     // NOTICE: This function is set in the constructor, and is passed in from the top level Chatbot component. The setState function here actually manipulates the top level state of the Chatbot, so it's important that we make sure that we preserve the previous state.
--- src/MessageParser.js
+++ src/MessageParser.js
@@ -4,19 +4,27 @@ class MessageParser {
     this.actionProvider = actionProvider;
   }
 
-  parse(message) {
+  async parse(message) {
     const lowerCaseMessage = message.toLowerCase();
     console.log(lowerCaseMessage);
     if (lowerCaseMessage.includes("hello") || lowerCaseMessage.includes("hi")) {
       this.actionProvider.greet();
-    }
-
-    if(lowerCaseMessage.includes("Yes, I'm ready")){
+    } else if (lowerCaseMessage.includes("Yes, I'm ready")) {
       this.actionProvider.recentlyFeeling();
-    }
+    } else {
+      const sendMessage = async () => {
+        const response = await fetch('https://ddp-ai-kpglnlidnq-ts.a.run.app/add_message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name: 'test', message: message })
+        });
+        const data = await response.json();
+        this.actionProvider.handleResponse(data.message);
+      };
 
-    if (lowerCaseMessage.includes("javascript")) {
-      this.actionProvider.handleJavascriptList();
+       await sendMessage();
     }
   }
 }
